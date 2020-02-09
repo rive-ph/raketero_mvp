93c93794f5b99c87b186f15bffca85501bd4a423
Kim Avillanosa <avillanosakzxc@gmail.com>
2020-02-09 18:05:48 +0800

    Added additional Entities

---
+++ raketero_api/Raketero.Entities/BusinessJobPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raketero.Entities
+{
+    public enum BusinessJobPostStatus
+    {
+        Active = 1, 
+        Inactive = 2
+    }
+
+    public class BusinessJobPost
+    {
+        public string Description { get; set; }
+        public DateTime PostDate { get; set; }
+        public BusinessJobPostStatus BusinessJobPostStatus { get; set; }
+    }
+}
